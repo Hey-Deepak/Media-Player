ca0a2c77358fe669b35ea2ce0f8b8505acde2f8a
Hey-Deepak <choudharydeepak990@gmail.com>
2020-03-23 14:35:59 +0530

    Update MainActivity.java

--- app/src/main/java/com/dc/mediaplayer/MainActivity.java
+++ app/src/main/java/com/dc/mediaplayer/MainActivity.java
@@ -2,7 +2,10 @@ package com.dc.mediaplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button bPlay = (Button)findViewById(R.id.play);
+        Button bPause = (Button)findViewById(R.id.pause);
+
+        final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.Janudi);
+
+        bPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.start();
+            }
+        });
+
+        bPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.stop();
+            }
+        });
     }
 }
