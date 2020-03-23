08ba2e7b808ac3ff9ccdcf099602e99dec5a4da2
Hey-Deepak <choudharydeepak990@gmail.com>
2020-03-23 14:35:45 +0530

    GDU

--- app/src/main/java/MediaPlayer.java
+++ app/src/main/java/MediaPlayer.java
@@ -2,7 +2,7 @@ import com.dc.mediaplayer.R;
 
 public class MediaPlayer extends android.media.MediaPlayer {
 
-    MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.song);
+//    MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.Janudi);
 
 
 }
